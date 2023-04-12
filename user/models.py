effb6f0e2042146300e03869f0f94db2b79c6ef8
Songjimyung <116045723+Songjimyung@users.noreply.github.com>
2023-04-12 14:27:34 +0900

    Revert "make user login"

--- user/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-from django.contrib.auth.models import AbstractUser
-
-# Create your models here.
-class UserModel(AbstractUser) :
-    class Meta:
-        db_table = 'user'
-
-    bio = models.TextField(max_length=256, default='')
--- user/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path, include
-from . import views
-
-urlpatterns = [
-    path("", views.home, name="home"),
-    path("sign-up/", views.sign_up_view, name="sign-up"),
-    path("sign-in/", views.sign_in_view, name="sign-in"),
-    path("logout/", views.logout, name="logout"),
-]
